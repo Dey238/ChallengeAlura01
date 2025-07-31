0b423bec12d25d653b64a4183cd6725432a77e12
Alexander <axdel238@gmail.com>
2025-07-31 11:19:26 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,42 @@
+let amigos = [];
+
+function agregarAmigo() {
+  const input = document.getElementById("amigo");
+  const nombre = input.value.trim();
+
+  if (nombre === "") {
+    alert("Por favor, inserta un nombre.");
+    return;
+  }
+
+  amigos.push(nombre);
+  mostrarLista();
+  input.value = "";
+}
+
+function mostrarLista() {
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("No hay amigos en la lista para sortear.");
+    return;
+  }
+
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  const amigoSorteado = amigos[indiceAleatorio];
+
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li>🎉 Amigo sorteado: <strong>${amigoSorteado}</strong></li>`;
+}
+
+
+
